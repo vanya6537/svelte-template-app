318a125957acb84ba1f9ed505b95144afc062c1b
Netsl <vanya6537@gmail.com>
2020-12-06 07:55:18 +0300

    Tests structure added

---
+++ src/components/__test__/App.test.js
@@ -0,0 +1,9 @@
+// import { render } from '@testing-library/svelte';
+// import AdaptiveInfo from '../AdaptiveInfo.svelte';
+
+describe('Component', () => {
+  test('Should render skeleton correctly', () => {
+    // const { container } = render(App({ target: document.body }));
+    // expect(container).toContainHTML('<main><header></header><div></div></main>');
+  });
+});
